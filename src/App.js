7b4e1f0a523c8ae9e7df3a30f1f5f74da8af0953
dev2yup <>
2022-03-27 03:16:00 +0900

    가운데 정렬, 푸터 헤더 합치기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
+import Header from './components/Header/Header'
 import './App.css';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
+    <>
+    <Header />
     <div className="App">
-      이동엽
-      리액트를 공부하고 있습니다.
+      이동엽 <br />
+      리액트를 공부하고 있습니다. <br />
       숭실대학교 소프트웨어학부 22학번 입니다.
     </div>
+    <Footer />
+    </>
   );
 }
 

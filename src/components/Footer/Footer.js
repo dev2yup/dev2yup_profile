e7825035cb8c72a4020a59d97be5e037b756afd1
dev2yup <>
2022-03-27 03:12:51 +0900

    푸터 만들어보기.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <>
+            <div className="FooterStyle">© {new Date().getFullYear()} Dev2yup all rights reserved.</div>
+        </>
+    )
+
+}
